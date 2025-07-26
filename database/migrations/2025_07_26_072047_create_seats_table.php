159585c0601115a76f91467901047fea6ccfae34
DhaniKWP <dhanikusuma256@gmail.com>
2025-07-26 14:21:24 +0700

    buat struktur table seats

---
+++ database/migrations/2025_07_26_072047_create_seats_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('seats', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('bus_id')->constrained()->onDelete('cascade');
+        $table->string('nomor_kursi'); // Misal: A1, B2, C3
+        $table->boolean('is_booked')->default(false);
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seats');
+    }
+};
